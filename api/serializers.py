2f02dd1f80706955ba46727e3ab1d83ff74e34b0
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2022-11-08 12:19:09 +0100

    initial commit

---
+++ api/serializers.py
@@ -0,0 +1,11 @@
+# serializer for note
+
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+    
